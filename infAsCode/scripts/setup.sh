ef187fd1ee540ee3707999a1d0b007414770972b
steinko <steinkor@gmail.com>
2021-04-12 07:22:14 +0200

    remove comments

--- infAsCode/scripts/setup.sh
+++ infAsCode/scripts/setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
-# exit if a command returns a non-zero exit code and also print the commands and their args as they are executed
 set -e -x
-# Download and install required tools.
-# pulumi
 curl -fsSL https://get.pulumi.com/ | bash
 export PATH=$PATH:$HOME/.pulumi/bin
 # Login into pulumi. This will require the PULUMI_ACCESS_TOKEN environment variable
