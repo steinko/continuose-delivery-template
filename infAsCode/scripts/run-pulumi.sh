9d87ce63b57ae00537a22f772fa0a60f8e0fb420
steinko <steinkor@gmail.com>
2021-04-15 08:33:28 +0200

    remove import

--- infAsCode/scripts/run-pulumi.sh
+++ infAsCode/scripts/run-pulumi.sh
@@ -9,5 +9,4 @@ export PATH=$PATH:$HOME/.pulumi/bin
 cd infAsCode
 npm install
 pulumi stack select dev
-pulumi import  --yes gitlab:index/projectCluster:ProjectCluster gradle-ci-cd 25815104:145188
 pulumi up --yes
