1d2bd33ae40e79acd9e10e2fc66edb2dc1f3e414
steinko <steinkor@gmail.com>
2021-04-15 09:07:06 +0200

    add python and google cloud

--- infAsCode/scripts/setup.sh
+++ infAsCode/scripts/setup.sh
@@ -10,3 +10,11 @@ apk add  nodejs
 curl -qL https://www.npmjs.com/install.sh | sh
 # yarn
 npm install npm@latest -g
+curl "https://pyenv.run" | bash
+mkdir gcloud-build && cd gcloud-build;
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-189.0.0-linux-x86_64.tar.gz;
+tar -xzf google-cloud-sdk-189.0.0-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh
+source google-cloud-sdk/path.bash.inc && echo "source google-cloud-sdk/path.bash.inc" >> $HOME/.profile
+gcloud config set project springboot22
+gcloud auth application-default login
