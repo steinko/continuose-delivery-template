f3abef3e0376ed08f1faf90ce0f398d7b3ad469a
steinko <steinkor@gmail.com>
2021-04-15 07:54:51 +0200

    add cluster

--- infAsCode/index.ts
+++ infAsCode/index.ts
@@ -1,5 +1,9 @@
 import * as k8s from "@pulumi/kubernetes";
 import * as kx from "@pulumi/kubernetesx";
+import * as gitlab from "@pulumi/gitlab";
+
+
+const projectCluster = gitlab.ProjectCluster.get("gradle-ci-cd-cluster", "145208")
 
 const appLabels = { app: "nginx" };
 const deployment = new k8s.apps.v1.Deployment("nginx", {
