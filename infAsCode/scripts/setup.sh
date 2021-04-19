ac2dabc48c525dad7eb174f17f253ecd2c28a9f9
steinko <steinkor@gmail.com>
2021-04-19 11:54:37 +0200

    fix images

--- infAsCode/scripts/setup.sh
+++ infAsCode/scripts/setup.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -e -x
+apt install curl
 curl -fsSL https://get.pulumi.com/ | bash
 export PATH=$PATH:$HOME/.pulumi/bin
 # Login into pulumi. This will require the PULUMI_ACCESS_TOKEN environment variable
 
 pulumi login
 # nodejs
-apk add  nodejs
+apt install  nodejs
 curl -qL https://www.npmjs.com/install.sh | sh
 # yarn
 npm install npm@latest -g
 
 mkdir gcloud-build && cd gcloud-build;
-wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-189.0.0-linux-x86_64.tar.gz;
+curl -fsSL https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-189.0.0-linux-x86_64.tar.gz| bash
 tar -xzf google-cloud-sdk-189.0.0-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh
 source google-cloud-sdk/path.bash.inc && echo "source google-cloud-sdk/path.bash.inc" >> $HOME/.profile
