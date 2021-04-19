e54d393a76b51f1e55f77088019ba34b24cba883
steinko <steinkor@gmail.com>
2021-04-19 16:37:59 +0200

    add update

--- infAsCode/scripts/setup.sh
+++ infAsCode/scripts/setup.sh
@@ -17,6 +17,7 @@ npm install npm@latest -g
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" |tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 apt install apt-transport-https ca-certificates gnupg -y
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg |apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+apt update -y
 apt install google-cloud-sdk -y
 gcloud init
 gcloud config set project springboot22
