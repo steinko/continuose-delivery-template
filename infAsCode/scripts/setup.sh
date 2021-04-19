02eedc50f8df02d18389dc83a5577a2808df7c1d
steinko <steinkor@gmail.com>
2021-04-19 16:28:11 +0200

    removed sudo

--- infAsCode/scripts/setup.sh
+++ infAsCode/scripts/setup.sh
@@ -14,11 +14,11 @@ curl -qL https://www.npmjs.com/install.sh | sh
 # yarn
 npm install npm@latest -g
 
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-sudo apt-get install apt-transport-https ca-certificates gnupg
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt-get update && sudo apt-get install google-cloud-sdk
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" |tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+apt install apt-transport-https ca-certificates gnupg -y
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg |apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+apt install google-cloud-sdk -y
 gcloud init
 gcloud config set project springboot22
 gcloud auth activate-service-account --key-file= /springboot22-032a69ee7f66.json
-cd ..
+
