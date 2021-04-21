953d20a374f81618989b3f56b47565ae0427a99e
steinko <steinkor@gmail.com>
2021-04-21 11:09:04 +0200

    move auth

--- infAsCode/scripts/setup.sh
+++ infAsCode/scripts/setup.sh
@@ -21,10 +21,9 @@ apt install apt-transport-https ca-certificates gnupg -y
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg |apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 apt update -y
 apt install google-cloud-sdk -y
-gcloud config set account  serviceaccount@springboot22.iam.gserviceaccount.com
+gcloud auth activate-service-account --key-file=scripts/springboot22-0b39b2fdd02b.json
 gcloud config set project springboot22
 gcloud config set compute/zone europe-north1-a
-gcloud auth activate-service-account --key-file=scripts/springboot22-0b39b2fdd02b.json
 apt install kubectl
 gcloud services enable cloudresourcemanager.googleapis.com
 kubectl create secret generic servic-account-key --from-file=key.json=springboot22-032a69ee7f66.json
