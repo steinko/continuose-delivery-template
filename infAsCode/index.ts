770e5eaa50c514407b080c06c014d17e0013c6a2
steinko <steinkor@gmail.com>
2021-05-07 07:15:05 +0200

    update infrastructure

--- infAsCode/index.ts
+++ infAsCode/index.ts
@@ -1,53 +1,30 @@
 import * as k8s from "@pulumi/kubernetes";
-import * as pulumi from "@pulumi/pulumi";
 import {clusterProvider} from "./Cluster"
 
-
-export const nameSpace =  new k8s.core.v1.Namespace("staging",undefined,{provider: clusterProvider})
+const name = "hello-world"
+export const nameSpace =  new k8s.core.v1.Namespace('staging',{},{provider: clusterProvider})
 export const nameSpaceName =  nameSpace.metadata.name;
 
 
-let config = new pulumi.Config();
-let dockerName = config.require("dokcerName")
-let dockerPassword = config.require("dokcerPassword")
-let registry = config.require("registry")
 
-
-    // Put the username password into dockerconfigjson format.
-    let base64JsonEncodedCredentials : pulumi.Output<string> = 
-        pulumi.all([dockerName, dockerPassword, registry])
-        .apply(([username, password, registry]) => {
-            const base64Credentials = Buffer.from(username + ':' + password).toString('base64')
-            const json =  `{"auths":{"${registry}":{"auth":"${base64Credentials}"}}}`
-            console.log(json)
-            return Buffer.from(json).toString('base64')
-        })
-
-const dockeSeceret = new k8s.core.v1.Secret('dokcerl-secret', {
-        metadata: {
-            name: 'docker--secret',
-            namespace: nameSpaceName
-        },
-        type: 'kubernetes.io/dockerconfigjson',
-        data: {
-            ".dockerconfigjson": base64JsonEncodedCredentials,
-        },
-    }, { provider: clusterProvider })
-		
-
-
-export const deployment = new k8s.apps.v1.Deployment("hello-world-deployment", {
-	metadata: { labels: {app: "hello-world"},
-                         namespace: nameSpaceName },
+const appLabels = { appClass: name }
+export const deployment = new k8s.apps.v1.Deployment(name, {
+	metadata: { labels: appLabels,
+                namespace: nameSpaceName 
+              },
     spec: {
         replicas: 1,
-         selector: { matchLabels: {app: "hello-world"} },
+         selector: { matchLabels: appLabels  },
         template: {
+	       metadata: {
+                    labels: appLabels,
+                },
             
             spec: { 
 	                     
-	           containers: [{ name: "hello-world-image",
+	           containers: [{ name: name,
                               image: "docker.io/steinko/gradle-ci-cd",
+                               ports: [{ name: "http", containerPort: 8080 }],
                               livenessProbe:{ httpGet:{path:'/actuator/health/liveness',
                                                        port: 8080},
                                               initialDelaySeconds:5,
@@ -68,17 +45,26 @@ export const deployment = new k8s.apps.v1.Deployment("hello-world-deployment", {
     }
 },{provider: clusterProvider});
 
+export const deploymentName = deployment.metadata.name;
 
-export const service = new k8s.core.v1.Service('service', 
-{metadata: { name: 'hello-world-service',
-             namespace: nameSpaceName },
-spec:{ selector: {app: 'hello-world' } ,
-        ports: [{port: 8080, targetPort: 8080 } ],
-        type: 'LoadBalancer'} }, {provider: clusterProvider} ) 
+export const service = new k8s.core.v1.Service(name, 
+   {
+	  metadata: { 
+		 labels: appLabels,
+         namespace: nameSpaceName 
+      },
+      spec: { 
+	           type: 'LoadBalancer',
+               ports: [{port: 8080, targetPort: "http" } ],
+	           selector: appLabels 
+             } ,
+         }, 
+        {provider: clusterProvider} 
+  ) 
+export const serviceName = service.metadata.name;
+export const servicePublicIP = service.status.loadBalancer.ingress[0].ip
 
-const serviceAccount = new k8s.core.v1.ServiceAccount('service-account',
-                            {automountServiceAccountToken:false},
-                            {provider: clusterProvider});
+
 const hostname = "*" + '.steinko.com'
 const ingress = new k8s.networking.v1beta1.Ingress('ingress', 
           { 
