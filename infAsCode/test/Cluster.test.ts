029cbfbf089087485ddcee6a877eaf882d0b2dc1
steinko <steinkor@gmail.com>
2021-05-12 14:15:23 +0200

    add infrastucture as code unit test

--- infAsCode/test/Cluster.test.ts
+++ infAsCode/test/Cluster.test.ts
@@ -1,5 +1,38 @@
-const cluster = require('../Cluster');
+import * as pulumi from "@pulumi/pulumi"
 
-it('adds 1 + 2 to equal 3', () => {
-  expect(cluster.name).toBe('gradle-ci-cd');
-});
+
+pulumi.runtime.setMocks({
+	         newResource: function(args: pulumi.runtime.MockResourceArgs):{id: string, state: any} {
+	
+                return {
+                    id: args.inputs.name + "_id",
+                    state: args.inputs,
+         
+                };
+},
+ call: function(args: MockCallArgs) {
+        return args.input;
+    },
+})
+
+
+describe('', () => {
+	  let cluster: typeof import("../Cluster");
+
+   beforeAll(async () => {
+	    pulumi.runtime.setConfig('project:cloudLocation','europe-nort1-a')
+	    pulumi.runtime.setConfig('project:cloudRegion','europe-nort1')
+	    pulumi.runtime.setConfig('project:cloudProject','springboot22')
+        cluster = await import("../Cluster");
+       
+   })
+
+  it('empty test', async () => {
+	    
+       pulumi.all([cluster.cluster.name]).apply(([name]) => {
+     
+	        expect(name).toBe('gradle-ci-cd-cluster')
+       })
+  })
+
+})
