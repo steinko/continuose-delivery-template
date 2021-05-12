e47dbd96425296e72cfe85290e2c7d36018dcdc7
steinko <steinkor@gmail.com>
2021-05-12 18:59:14 +0200

    fix file path

--- infAsCode/scripts/run-pulumi.sh
+++ infAsCode/scripts/run-pulumi.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-cd infAsCode
+
 # exit if a command returns a non-zero exit code and also print the commands and their args as they are executed
 set -e -x
 
 # Add the pulumi CLI to the PATH
 export PATH=$PATH:$HOME/.pulumi/bin
-chmod +x /infAsCode/scripts/curl-install.sh
-/infAsCode/scripts/curl-install.sh
+chmod +x ./infAsCode/scripts/curl-install.sh
+./infAsCode/scripts/curl-install.sh
 
-
+cd infAsCode
 npm install
 
 export GOOGLE_CREDENTIALS=$(cat scripts/springboot22-0b39b2fdd02b.json)
