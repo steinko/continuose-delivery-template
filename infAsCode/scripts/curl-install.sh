338c12b0e628f8163657da470b479b52b6b63c6a
steinko <steinkor@gmail.com>
2021-05-12 19:55:12 +0200

    iserted istall.sh

--- infAsCode/scripts/curl-install.sh
+++ infAsCode/scripts/curl-install.sh
@@ -4,4 +4,10 @@ set -e -x
 apt update -y
 apt upgrade -y
 apt install curl -y
-npm-install.sh
+#!/bin/bash
+# nodejs
+apt install  nodejs -y
+curl -qL https://www.npmjs.com/install.sh | sh
+# yarn
+npm install npm@latest -g
+npm install
