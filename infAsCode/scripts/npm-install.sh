dda22858ebe74d0ef28a1484d3f0c81c990a105c
steinko <steinkor@gmail.com>
2021-05-12 16:14:59 +0200

    curl-install

--- infAsCode/scripts/npm-install.sh
+++ infAsCode/scripts/npm-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # nodejs
 
-chmod +x ./curl-install.sh
-./curl-install.sh
+chmod +x curl-install.sh
+curl-install.sh
 apt install  nodejs -y
 curl -qL https://www.npmjs.com/install.sh | sh
 # yarn
