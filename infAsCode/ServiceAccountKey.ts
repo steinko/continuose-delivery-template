7174b8a19cf139481625ef867504c5fe7cdb0ebe
steinko <steinkor@gmail.com>
2021-05-27 15:06:29 +0200

    add gcloud

--- infAsCode/ServiceAccountKey.ts
+++
@@ -1,7 +0,0 @@
-import * as gcp from "@pulumi/gcp";
-import {serviceAccount} from "./GcpServiceAccount"
-
-export const serviceAccountKey = new gcp.serviceaccount.Key("service-account-key", {
-    serviceAccountId: serviceAccount.name,
-    publicKeyType: "TYPE_X509_PEM_FILE",
-}, {parent: serviceAccount, additionalSecretOutputs: ["privateKey"]});
