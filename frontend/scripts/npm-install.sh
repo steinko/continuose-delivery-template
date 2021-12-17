8807923afdb7064016d48881ff9b4099f7e4487a
steinko <steinkor@gmail.com>
2021-12-17 19:36:58 +0100

    fix CI/CD script

--- frontend/scripts/npm-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# nodejs
-chmod +x ./scripts/curl-install.sh
-./scripts/curl-install.sh
-apt install  nodejs -y
-curl -qL https://www.npmjs.com/install.sh | sh
-# yarn
-npm install npm@latest -g
-npm install
