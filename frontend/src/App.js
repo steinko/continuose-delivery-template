483134350e5bb1deed0c18cbf863ffae8db47488
steinko <steinkor@gmail.com>
2021-12-25 05:08:55 +0100

    message as state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,36 @@
-import './App.css';
-import React  from 'react';
 
-function App() {
-  return (
-    <div>
-      <form>
-         <label>
-            Message:
-            <input type="text" name="message" readOnly value= "Hello World"/>
-        </label>
-      </form>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      message: "Hello World"
+    }
+  }
+	
+  getMessage() {
+	this.setStat({
+		message: "Hello World"
+	})
+  }
+
+  componetDidMount() {
+	this.getMessage()
+  }
+
+  render() {
+     return (
+        <div>
+          <form>
+             <label>
+                Message:
+                <input type="text" name="message" readOnly value= {this.state.message}/>
+             </label>
+           </form>
+         </div>
+      )
+    }
 }
 
 export default App;
